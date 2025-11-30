73ab0a33bcb529fab0f031e0f9c8f9c9810099c8
𝚴 𝐎 𝐁 𝚰 𝐓 𝚲 <nobitaxmusicbot@gmail.com>
2025-11-30 13:51:44 +0530

    Update settings.py

--- NOBITA_MUSIC/plugins/bot/settings.py
+++ NOBITA_MUSIC/plugins/bot/settings.py
@@ -54,7 +54,7 @@ async def settings_mar(client, message: Message, _):
 @languageCB
 async def gib_repo(client, CallbackQuery, _):
     await CallbackQuery.edit_message_media(
-        InputMediaVideo("https://files.catbox.moe/s7e5tu.mp4"),
+        InputMediaVideo("https://t.me/ll_MORTAL_PAPA_DEFAULTERS_ll"),
         reply_markup=InlineKeyboardMarkup(
             [[InlineKeyboardButton(text="ʙᴀᴄᴋ", callback_data=f"settingsback_helper")]]
         ),
